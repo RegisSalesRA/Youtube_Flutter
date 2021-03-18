6fae5063034e88bde80d7a9c7fb0296c5be3591d
RegisSalesRA <regismsales@gmail.com>
2021-03-18 19:38:30 -0300

    Appbar with icon and buttons

---
+++ lib/home.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.grey),
+        backgroundColor: Colors.white,
+        title: Image.asset(
+          "imagens/youtube.png",
+          width: 98,
+          height: 22,
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.videocam),
+            onPressed: () {
+              print("Acao video funcionando");
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              print("pesquisa");
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.account_circle),
+            onPressed: () {
+              print("conta");
+            },
+          ),
+        ],
+      ),
+      body: Container(),
+    );
+  }
+}
