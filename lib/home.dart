865c3574c2d9ae8fdbe4e33edf7272957d19bfb8
RegisSalesRA <regismsales@gmail.com>
2021-03-18 20:42:00 -0300

    Bottom with icons and links

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,7 @@
+import 'package:Flutter_youtube_api/telas/biblioteca.dart';
+import 'package:Flutter_youtube_api/telas/emalta.dart';
+import 'package:Flutter_youtube_api/telas/inicio.dart';
+import 'package:Flutter_youtube_api/telas/inscricao.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -6,8 +10,17 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int _indiceAtual = 0;
+
   @override
   Widget build(BuildContext context) {
+    List<Widget> telas = [
+      Inicio(),
+      EmAlta(),
+      Inscricao(),
+      Biblioteca(),
+    ];
+
     return Scaffold(
       appBar: AppBar(
         iconTheme: IconThemeData(color: Colors.grey),
@@ -38,7 +51,37 @@ class _HomeState extends State<Home> {
           ),
         ],
       ),
-      body: Container(),
+      body: telas[_indiceAtual],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _indiceAtual,
+        onTap: (indice) {
+          setState(() {
+            _indiceAtual = indice;
+          });
+        },
+//        type: BottomNavigationBarType.shifting,
+        type: BottomNavigationBarType.fixed,
+        fixedColor: Colors.red,
+
+        items: [
+          BottomNavigationBarItem(
+//              backgroundColor: Colors.orange,
+              title: Text("Inicio"),
+              icon: Icon(Icons.home)),
+          BottomNavigationBarItem(
+              //            backgroundColor: Colors.green,
+              title: Text("Em alta"),
+              icon: Icon(Icons.whatshot)),
+          BottomNavigationBarItem(
+              //          backgroundColor: Colors.blue,
+              title: Text("Inscrições"),
+              icon: Icon(Icons.subscriptions)),
+          BottomNavigationBarItem(
+              //        backgroundColor: Colors.pink,
+              title: Text("Biblioteca"),
+              icon: Icon(Icons.folder)),
+        ],
+      ),
     );
   }
 }
