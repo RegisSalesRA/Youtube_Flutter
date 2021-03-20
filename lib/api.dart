074eeafc2d925d172467ea88993d51d76775c74d
RegisSalesRA <regismsales@gmail.com>
2021-03-20 00:54:01 -0300

    convertendo resultado da api

--- lib/api.dart
+++ lib/api.dart
@@ -1,6 +1,7 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'dart:convert';
+
+import 'model/video.dart';
 
 const CHAVE_YOUTUBE_API = "AIzaSyBms4rk-PF_VaM2cUAopcIOc4zpvPcI8Cg";
 const ID_CANAL = "UCW3T8dQVEynwj6-_DArcM7w";
@@ -19,6 +20,20 @@ class Api {
             "&q=$pesquisa");
     if (response.statusCode == 200) {
       Map<String, dynamic> dadosJson = json.decode(response.body);
+
+      List<Video> videos = dadosJson["items"].map<Video>((map) {
+        return Video.fromJson(map);
+      }).toList();
+
+      for (var video in videos) {
+        print("resultado: " + video.titulo);
+      }
+
+/*
+      for (var video in dadosJson["items"]) {
+        print("Resultado: " + video.toString());
+      }
+*/
       print(
           "Resultado: " + dadosJson["items"][0]["snippet"]["title"].toString());
     } else {}
--- lib/model/video.dart
+++ lib/model/video.dart
@@ -1,9 +1,18 @@
 class Video {
   String id;
-  String video;
+  String titulo;
   String descricao;
   String imagem;
   String canal;
 
-  Video({this.id, this.video, this.descricao, this.imagem, this.canal});
+  Video({this.id, this.titulo, this.descricao, this.imagem, this.canal});
+
+  factory Video.fromJson(Map<String, dynamic> json) {
+    return Video(
+      id: json["id"]["videoId"],
+      titulo: json["snippet"]["title"],
+      imagem: json["snippet"]["thumbnails"]["high"]["url"],
+      canal: json["snippet"]["channelId"],
+    );
+  }
 }
