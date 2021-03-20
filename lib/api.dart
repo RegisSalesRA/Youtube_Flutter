7c3ffb92b84976d93b9fbdd39ebb849d3bbf23d5
RegisSalesRA <regismsales@gmail.com>
2021-03-20 04:24:58 -0300

    Listando videos no inicio.dart

--- lib/api.dart
+++ lib/api.dart
@@ -8,7 +8,7 @@ const ID_CANAL = "UCW3T8dQVEynwj6-_DArcM7w";
 const URL_BASE = "https://www.googleapis.com/youtube/v3/";
 
 class Api {
-  pesquisar(String pesquisa) async {
+  Future<List<Video>> pesquisar(String pesquisa) async {
     http.Response response = await http.get(URL_BASE +
         "search"
             "?part=snippet"
@@ -24,10 +24,12 @@ class Api {
       List<Video> videos = dadosJson["items"].map<Video>((map) {
         return Video.fromJson(map);
       }).toList();
-
+      return videos;
+/*
       for (var video in videos) {
         print("resultado: " + video.titulo);
       }
+*/
 
 /*
       for (var video in dadosJson["items"]) {
